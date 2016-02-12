3ff5d07c1c46f8f28991482ea415b7008b348ffa
Seva <fostersmix@gmail.com>
2016-02-12 15:35:27 +0300

    test

---
+++ pay/include.php
@@ -0,0 +1,16 @@
+<?php
+
+$blockchain_root = "https://blockchain.info/";
+$blockchain_receive_root = "https://api.blockchain.info/";
+$mysite_root = "http://fostersmix.github.io";
+$secret = "123456789abc";
+$my_xpub = "xpub6Bxgb2629tWYnYe8Le12CrZrd1FTBYUP87eqr5AYgytD1LueD2mNHD1DaGASGwGJ5nQgEsTyGmmX7RTZPySTVMDzn1G7bPqet6FFNph9BPh";
+$my_api_key = "4f46c7fc-2df4-4c2f-941b-8db479ab7c39";
+
+//Database
+$mysql_host = 'http://dez.lublino.ru/phpmyadmin/';
+$mysql_username = 'blockchain';
+$mysql_password = 'blockchain';
+$mysql_database = 'blockchain';
+
+?>
