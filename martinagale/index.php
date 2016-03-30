2900b84330f1dfb6b1431ee6ce8ad38dad60d822
Seva <fostersmix@gmail.com>
2016-03-30 21:07:41 +0300

    ups

---
+++ martinagale/index.php
@@ -0,0 +1,58 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+    <link rel="stylesheet" href="flexboxgrid.css" type="text/css">
+  </head>
+  <body>
+
+    <div class="row center-xs center-sm center-md center-lg middle-xs middle-sm middle-md middle-lg bg screen">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="box">
+          <div class="row center-xs center-sm center-md center-lg">
+            <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+              <div class="box">
+                <div class="row center-xs center-sm center-md start-lg">
+                  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                    <div class="box top-10 bottom-10 white"><b>#usemartingale</b></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1"></div>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <div class="box white"><a href=""><?php include 'left/index.php';?></div>
+        <div class="box top-10 white"><i>L</i></div>
+      </div>
+      <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
+        <div class="box top-10 bottom-10 white" id="gray">or</i></div>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <div class="box white"><?php include 'right/index.php';?></div>
+        <div class="box top-10 white"><i>R</i></div>
+      </div>
+      <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1"></div>
+      <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+        <div class="box">
+          <div class="row center-xs center-sm center-md start-lg">
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-1">
+              <div class="box top-10 bottom-10 white"><a href="#" class="solid"><strike>stats</strike></a></div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-2 ">
+              <div class="box top-10 bottom-10 white"><a href="/bet/2016/#<?php echo date('dm', strtotime('-1 days'));?>" class="button">results</a></div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-1 first-lg">
+              <div class="box top-10 bottom-10 white"><a href="/about" class="solid">about</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+  <footer>
+  </footer>
+</html>
--- martingale/about/index.php
+++ martingale/about/index.php
@@ -1,7 +1,7 @@
 <html>
   <head>
-    <link rel="stylesheet" href="/bet/styles.css" type="text/css">
-    <link rel="stylesheet" href="/bet/flexboxgrid.css" type="text/css">
+    <link rel="stylesheet" href="/styles.css" type="text/css">
+    <link rel="stylesheet" href="/flexboxgrid.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   </head>
   <body>
