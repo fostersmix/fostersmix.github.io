41aeba9f308eb82718fe65635f39ebc6332555cb
Seva <fostersmix@gmail.com>
2017-03-09 03:42:26 +0300

    upload old projects

--- com.adobe.SheetHappens/jsx/functions/placing.jsx
+++
@@ -1,8 +0,0 @@
-﻿function placing() {
-    if (app.documents.length > 0 && app.activeDocument.pageItems.length === 0) {
-        app.executeMenuCommand("AI Place");
-        } else {
-            app.documents.add();
-            app.executeMenuCommand("AI Place");
-            }
-        }
