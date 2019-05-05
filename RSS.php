6b7a70282f6f6b930891f7b15589d12b780bf6b4
Seva <fostersmix@gmail.com>
2019-05-05 13:18:27 +0300

    del

--- RSS.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $url = 'https://twitrss.me/twitter_user_to_rss/?user=fostersmix';
-  $rss = simplexml_load_file($url);
-  //var_dump($rss->channel->item);
-  $item = $rss->channel->item;
-?>
-
-<?php echo $item->title; ?>
